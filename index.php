3ae757901f4b4c73254ba5e166f344afb11093f0
smlparry <samuel.parry@bigpond.com>
2015-02-05 21:48:30 +1100

    First Commit
    Incredibly simple coming soon page

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Coming Soon</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div class="content">
+			<h1><?php echo $_SERVER['SERVER_NAME'] ?> is coming soon!</h1>
+		</div>
+	</body>
+</html>
